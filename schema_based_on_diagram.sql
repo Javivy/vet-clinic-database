bd1b0c94d256cd76f3a7c4a3e86c4151712b8a5f
Javivy <84439052+Javivy@users.noreply.github.com>
2023-04-04 16:47:51 -0400

    Create invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,11 @@ CREATE TABLE patients (
   date_of_birth date,
   PRIMARY KEY(id)
 );
+
+CREATE TABLE invoices (
+  id int GENERATED ALWAYS AS IDENTITY,
+  total_amount decimal,
+  generated_at timestamp,
+  payed_at timestamp,
+  medical_history_id int
+);
