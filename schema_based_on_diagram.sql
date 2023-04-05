703dbeac322ed60abf7ee472223deb16df94282f
Javivy <84439052+Javivy@users.noreply.github.com>
2023-04-04 20:08:26 -0400

    Create indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,6 +11,7 @@ CREATE TABLE invoices (
   generated_at timestamp,
   payed_at timestamp,
   medical_history_id int,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
   PRIMARY KEY(id)
 );
 
@@ -22,6 +23,7 @@ CREATE TABLE invoice_items (
   invoice_id int,
   treatment_id int,
   FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id),
   PRIMARY KEY(id) 
 );
 
@@ -47,6 +49,8 @@ CREATE TABLE medical_history_treatment (
 );
 
 CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX treatment_id_index ON invoice_items(treatment_id);
 CREATE INDEX patient_id_index ON medical_histories(patient_id);
-CREATE INDEX medical_history_id_index ON medical_history_treatment(medical_history_id);
-CREATE INDEX treatment_id_index ON medical_history_treatment(treatment_id);
+CREATE INDEX medical_history_id_index ON invoices(medical_history_id);
+CREATE INDEX medical_history_id_index_join ON medical_history_treatment(medical_history_id);
+CREATE INDEX treatment_id_index_join ON medical_history_treatment(treatment_id);
